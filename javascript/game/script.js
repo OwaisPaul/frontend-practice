840aca382820a8d6bf3576dc7625940fc457a4ff
OwaisPaul <ovaish25@gmail.com>
2025-07-08 11:31:11 +0530

    added score

--- javascript/game/script.js
+++ javascript/game/script.js
@@ -1,21 +1,31 @@
     let humanChoice;
     const choices = ['rock', 'paper', 'scissors'];
 
+    const score = {
+      wins: 0,
+      losses: 0,
+      ties: 0
+    }
+
     function playGame(humanChoice) {
       const computerChoice = choices[Math.floor(Math.random() * choices.length)];
       alert(`Computer chose: ${computerChoice} , you chose: ${humanChoice}`);
       
 
       if (computerChoice === humanChoice) {
-        alert('Tie');
+        alert(`Tie
+           wins  ${score.wins += 1} losses ${score.losses} ties ${score.ties}`);
       } else if (
         (computerChoice === 'rock' && humanChoice === 'paper') ||
         (computerChoice === 'paper' && humanChoice === 'scissors') ||
         (computerChoice === 'scissors' && humanChoice === 'rock')
       ) {
-        alert(`${humanChoice} beats ${computerChoice}. You won!`);
+         alert(`${humanChoice} beats ${computerChoice}. You won!
+        wins  ${score.wins += 1} losses ${score.losses} ties ${score.ties}`);
       } else {
-        alert(`${computerChoice} beats ${humanChoice}. You lost.`);
+        alert(`${computerChoice} beats ${humanChoice}. You lost.
+         wins ${score.wins} losses ${score.losses += 1} ties ${score.ties}`);
+       
       }
     }  
 
